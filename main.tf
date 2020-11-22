558f3676a91052678c0804646e3cbddb0e2da2b8
Nisarg Thakkar <nisarg.tk@gmail.com>
2020-11-22 15:40:31 +0530

    First Commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider aws {
+  region = "ap-south-1"
+  profile = "default"
+}
+resource aws_instance "myec2" {
+  ami        = data.aws_ami.latest-ubuntu.id
+  instance_type = var.instance_type
+}
+data "aws_ami" "latest-ubuntu" {
+most_recent = true
+owners = ["099720109477"] # Canonical
+
+  filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
+}
